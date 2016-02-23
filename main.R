55b400fc5f2b116dbe21cc4734f891e6b9e6518c
vsynych <vs_mail@yahoo.com>
2016-02-22 22:09:28 -0500

    Commit with RStudio closed

---
+++ main.R
@@ -0,0 +1,13 @@
+dat <- c(1, 2, 3, 4, 5)
+names(dat) <- c("one", "two", "three", "four", "five")
+dat
+
+# And now a little bit of editing
+
+dat
+dat_y <- c(9, 8, 7, 6, 5)
+names(dat_y) <- c("nine", "eight", "seven", "six", "five")
+dat_y
+
+# New editing
+# New line
